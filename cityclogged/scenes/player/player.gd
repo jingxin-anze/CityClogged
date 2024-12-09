5c1bb57945acb91c0ac03e2acec62dffdc3a8795
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-09 22:14:48 +0800

    搭建框架

---
+++ cityclogged/scenes/player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+@onready var pitch: Node3D = %Pitch
+
+var movement_speed:float=20
+var mouse_sensitivity:int=400
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	var movement_direction:Vector2 = Input.get_vector("move_backward", "move_forward", "move_left", "move_right")
+	var movement_direction_3d:Vector3 = basis.x * movement_direction.y  - basis.z * movement_direction.x
+	velocity = movement_direction_3d *movement_speed
+	move_and_slide()
+
+func _input(event:InputEvent) -> void:
+	if not event is InputEventMouseMotion:
+		return
+		
+	var mouse_movement:Vector2 = event.relative /mouse_sensitivity * PI
+	self.rotate_y(-mouse_movement.x )
+	pitch.rotate_x(-mouse_movement.y )
