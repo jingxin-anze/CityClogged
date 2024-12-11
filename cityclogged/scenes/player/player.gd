bc7d3902c2ec786225f91da156c5890979c599fa
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-11 23:41:24 +0800

    固定相机

--- cityclogged/scenes/player/player.gd
+++ cityclogged/scenes/player/player.gd
@@ -23,9 +23,9 @@ func _input(event:InputEvent) -> void:
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
 		
-	if not event is InputEventMouseMotion:
-		return
-	
-	var mouse_movement:Vector2 = event.screen_relative /mouse_sensitivity * PI
-	self.rotate_y(-mouse_movement.x )
-	pitch.rotate_x(-mouse_movement.y )
+	#if not event is InputEventMouseMotion:
+		#return
+	#
+	#var mouse_movement:Vector2 = event.screen_relative /mouse_sensitivity * PI
+	#self.rotate_y(-mouse_movement.x )
+	#pitch.rotate_x(-mouse_movement.y )
