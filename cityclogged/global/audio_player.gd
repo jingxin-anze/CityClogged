ea255daa425278554d3144750ef5ab205ea71a33
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-14 15:13:53 +0800

    优化旋转视角1.0

--- cityclogged/global/audio_player.gd
+++ cityclogged/global/audio_player.gd
@@ -1,1 +1,70 @@
 extends Node
+
+##播放音频
+func play(stream:AudioStream, single:bool=false,is_release:bool=true,volume:float=0,about_position:bool=false) -> Node:
+	if single:
+		# 获取所有的audio节点
+		var audio_list = get_children()
+		for audio_node in audio_list:
+			if audio_node.stream.resource_path == stream.resource_path:
+				audio_node.stream_paused=false
+				return audio_node
+				
+	#创建播放器
+	var audio=AudioStreamPlayer.new()
+	var audio1=AudioStreamPlayer2D.new()
+	#传入音频文件
+	if not about_position:
+		audio.volume_db=volume
+		audio.stream=stream
+		audio.finished.connect(_finished.bind(audio,is_release))
+		add_child(audio)
+		audio.playing=true
+		return audio
+	else:
+		audio1.volume_db=volume
+		audio1.stream=stream
+		audio1.finished.connect(_finished.bind(audio1,is_release))
+		add_child(audio1)
+		audio1.playing=true
+		return audio1
+
+##暂停此播放器
+func pause(audio):
+	audio.stream_paused=true
+
+##让暂停的播放器继续播放
+func contine(audio):
+	audio.stream_paused=false
+
+##销毁此播放器
+func destory(audio):
+	audio.stop()
+	audio.queue_free()
+
+#此函数的参数是在连接信号的的时候传入的
+##音乐播放完自动执行
+func _finished(audio,is_release):
+	if is_release:
+		audio.queue_free()
+	audio.play()
+
+##清除所有音乐
+func clear():
+	if not get_children().is_empty():
+		var count=get_child_count()
+		for i in range(count):
+			var child=get_child(count-i-1)
+			child.call_deferred("queue_free")
+	else:
+		return
+
+##返回当前线程的线性音量 
+func get_volume(bus_index:int):
+	var db:=AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(db)
+
+##需要注意传入的值是线性的。
+func set_volume(bus_index:int,v:float):
+	var db:=linear_to_db(v)
+	AudioServer.set_bus_volume_db(bus_index,db)
--- cityclogged/global/save_load_system.gd
+++ cityclogged/global/save_load_system.gd
@@ -1,1 +1,63 @@
-extends Node
+extends Resource
+
+@export var archives_file:Dictionary={
+}
+
+var game_data_path:String="user://game_data.tres"
+var data:SLSystem
+
+##保存数据
+func save_data(key:String,value):
+	_init_file()
+	data.archives_file[key]=value
+	ResourceSaver.save(data, game_data_path)
+ 
+##加载数据
+func load_data(key:String):
+	if FileAccess.file_exists(game_data_path):
+		data=ResourceLoader.load(game_data_path) as SLSystem
+		if not (key in data.archives_file):
+			printerr("键 "+key+" 不存在")
+			return
+	else:
+		printerr("文件不存在")
+	return data.archives_file[key]
+
+##打印出所有数据
+func show_all_infro():
+	_init_file()
+	if archives_file:
+		print(data.archives_file)
+	else:
+		print("存档未写入任何内容")
+
+##打印出特定的键值对
+func show_certain_infro(key:String):
+	_init_file()
+	if key in data.archives_file:
+		print(key+" : "+"%d"%data.archives_file[key])
+	else:
+		printerr("键 "+key+" 不存在")
+
+##清空字典里的所有内容
+func clear_content():
+	_init_file()
+	data.archives_file.clear()
+	ResourceSaver.save(data, game_data_path)
+
+##清除字典里的特定内容
+func clear_certain_content(key:String):
+	_init_file()
+	if key in data.archives_file:
+		data.archives_file.erase(key)
+		ResourceSaver.save(data, game_data_path)
+	else:
+		printerr("键 "+key+" 不存在")
+
+##初始化文件，一般来说外界无需刻意调用
+func _init_file():
+	if FileAccess.file_exists(game_data_path):
+		data = ResourceLoader.load(game_data_path) as SLSystem
+	else:
+		data = SLSystem.new()
+		ResourceSaver.save(data, game_data_path)
