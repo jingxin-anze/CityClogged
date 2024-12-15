c75bddafb3a585955381e5883db0300898edfc09
jingxin-anze <2028667835@qq.com>
2024-12-15 14:15:23 +0800

    无

--- cityclogged/test_car_huayu/test_car.gd
+++ cityclogged/test_car_huayu/test_car.gd
@@ -1,46 +1,39 @@
+class_name CommonCar
 extends RigidBody3D
-#车辆面朝右
 
-#是否启动导航
-@export var navigation: bool = true
 #车辆一般的运动速度
 @export var speed: float = 5
 #与目的地的最小距离
-@export var stop_min: float = 1
+#@export var stop_min: float = 1
 #车辆的目的地，现在为一个点，以后可以为两个点（比如一个地方有两个停车场）或一片区域（一个数组）
+var stop_min = 5
 var target_one: Vector3 = Vector3.ZERO
 var direction_3d: Vector3 = Vector3.ZERO
-
+var current_path_3d: PackedVector3Array = []
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 func _ready() -> void:
-	if navigation == false:
-		navigation_agent_3d.queue_free()
+	await get_tree().current_scene.ready
+	navigation_agent_3d.target_position = target_one
 
 func _physics_process(delta: float) -> void:
-	#var direction_3d: = (navigation_agent_3d.get_next_path_position() - global_position).normalized()
-	#
-	#linear_velocity = direction_3d * speed
 	pass
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if navigation:
-		_navigation()
-	pass
-	
-
-func _navigation() -> void:
-	if target_one == Vector3.ZERO:
-		return
-	#print(self.rotation)
+	if ray_cast_3d.is_colliding():
+		linear_velocity = self.linear_velocity.slerp(0,0.02*10)
+	if navigation_agent_3d.get_current_navigation_path().size() == 0:
+		current_path_3d = navigation_agent_3d.get_current_navigation_path()
+	print(navigation_agent_3d.get_current_navigation_path())
 	if navigation_agent_3d.distance_to_target() < stop_min:
 		linear_velocity = Vector3(0,0,0)
 		angular_velocity = Vector3(0,0,0)
 		queue_free()
 		return
+		
 	direction_3d = (navigation_agent_3d.get_next_path_position() - global_position).normalized()
-
+	
 	var direction_2d: = Vector2(direction_3d.z,direction_3d.x)
 	#print(navigation_agent_3d.get_next_path_position())
 	
@@ -48,9 +41,3 @@ func _navigation() -> void:
 	self.quaternion = self.quaternion.slerp(target_quaternion,0.02*10)
 	if direction_3d != Vector3.ZERO:
 		linear_velocity = direction_3d * speed
-
-
-func _on_timer_timeout() -> void:
-	if navigation:
-		navigation_agent_3d.target_position = target_one
-		navigation_agent_3d.target_position = navigation_agent_3d.get_final_position()
