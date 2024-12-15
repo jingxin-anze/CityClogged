774a9d2300ca10241c29f4371955a690112e532d
aidewn <50902442+aidewn@users.noreply.github.com>
2024-12-16 02:18:19 +0800

    新的地块

--- cityclogged/scenes/player/ray_cast_3d.gd
+++ cityclogged/scenes/player/ray_cast_3d.gd
@@ -1,7 +1,7 @@
 extends RayCast3D
 
 var player:Player
-
+var is_change: bool = true
 func _ready() -> void:
 	await owner.ready
 	player=owner
@@ -12,11 +12,18 @@ func _process(delta: float) -> void:
 	
 	if self.is_colliding():
 		var colled_entity:=self.get_collider()
-		if (colled_entity  is not Player) :
+		if (colled_entity  is not Player) && is_change:
 			player.rotation_degrees.y=90.0
+			is_change = false
 			return
 		#elif (colled_entity is Player):
 			#player.rotation_degrees.y=0.0
 	
-			
-		pass
+	if 	!is_change:
+		camera_raycast_player()
+
+
+func  camera_raycast_player():
+	var colled_entity:=self.get_collider()
+	if colled_entity  is Player:
+		is_change = true
