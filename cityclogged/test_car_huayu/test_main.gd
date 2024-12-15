7e8d89fcf2492dc4bd22139ad3d09929220cec60
jingxin-anze <2028667835@qq.com>
2024-12-15 14:36:34 +0800

    小车的简单运动1

--- cityclogged/test_car_huayu/test_main.gd
+++ cityclogged/test_car_huayu/test_main.gd
@@ -3,9 +3,9 @@ extends Node3D
 var s: Array = []
 
 @onready var target: Marker3D = $Target
-@onready var test_car: RigidBody3D = %TestCar
 @onready var navigation_region_3d: NavigationRegion3D = $NavigationRegion3D
+@onready var test_car_2: CommonCar = %TestCar2
 
 
 func _ready() -> void:
-	test_car.target_one = target.position
+	test_car_2.target_one = target.position
