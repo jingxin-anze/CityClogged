7138cf466faaec5168737bff0adb45776c7eecfb
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-16 20:02:32 +0800

    重新添加丢失的射线脚本

--- cityclogged/scenes/player/ray_cast_3d.gd
+++ cityclogged/scenes/player/ray_cast_3d.gd
@@ -1,7 +1,9 @@
 extends RayCast3D
 
 var player:Player
-var is_change: bool = true
+var sprite:Sprite3D
+var is_first:bool=true
+
 func _ready() -> void:
 	await owner.ready
 	player=owner
@@ -12,18 +14,14 @@ func _process(delta: float) -> void:
 
 	if self.is_colliding():
 		var colled_entity:=self.get_collider()
-		if (colled_entity  is not Player) && is_change:
-			player.rotation_degrees.y=90.0
-			is_change = false
+		if (colled_entity  is not Player) and is_first:
+			sprite.no_depth_test=true
+			sprite.modulate=Color.BLACK
+			is_first=false
 			return
-		#elif (colled_entity is Player):
-			#player.rotation_degrees.y=0.0
-	
-	if 	!is_change:
-		camera_raycast_player()
-
-
-func  camera_raycast_player():
-	var colled_entity:=self.get_collider()
-	if colled_entity  is Player:
-		is_change = true
+		elif (colled_entity is Player):
+			is_first=true
+			sprite.no_depth_test=false
+			sprite.modulate=Color(1,1,1)
+			
+		pass
