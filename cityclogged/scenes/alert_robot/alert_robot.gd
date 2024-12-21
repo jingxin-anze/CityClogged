78f56a13f7c53d8a0c3648a5f3d6536a13173a61
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-21 16:50:27 +0800

    加一个插件

--- cityclogged/scenes/alert_robot/alert_robot.gd
+++ cityclogged/scenes/alert_robot/alert_robot.gd
@@ -1,9 +1,9 @@
 extends RigidBody3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not "," in "ve,":
+		print("ppp")
+	#print("Vector2(1,1)".split(","))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
