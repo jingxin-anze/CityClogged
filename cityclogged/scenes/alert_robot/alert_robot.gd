46c78841b265a8bde5df13ded4da97cfd2cee14a
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-29 21:19:08 +0800

    写完巡逻了

--- cityclogged/scenes/alert_robot/alert_robot.gd
+++ cityclogged/scenes/alert_robot/alert_robot.gd
@@ -1,6 +1,9 @@
-extends CharacterBody3D
+class_name AlertRobot extends CharacterBody3D
 
 var fall_speed:int=10
+var p1:Vector3
+var p2:Vector3
+@export var can_track:bool
 
 func _physics_process(delta: float) -> void:
 	#众所周知重力加速度为9.8米每秒的二次方，所以直接赋值了
