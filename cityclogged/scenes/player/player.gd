3943796021b315b3c747d08aa31c7f5ce4f9e2df
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-12-31 12:04:02 +0800

    机器人全视角转向

--- cityclogged/scenes/player/player.gd
+++ cityclogged/scenes/player/player.gd
@@ -9,9 +9,15 @@ var movement_speed:int=30
 var jump_speed:int=50
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-#var mouse_sensitivity:int=250
 var movement_direction:Vector2
 var movement_direction_3d:Vector3
+enum state{
+	z_p,
+	z_n,
+	x_p,
+	x_n,
+}
+var current_state=state.z_p
 
 func _ready() -> void:
 	if is1920:
@@ -23,6 +29,8 @@ func _ready() -> void:
 
 	movement_speed=self_attri.attri["movement_speed"]
 	jump_speed=self_attri.attri["jump_speed"]
+	
+	
 
 
 func _physics_process(delta: float) -> void:
@@ -34,6 +42,8 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y+=jump_speed*delta
 	move_and_slide()
+	#var a:Vector3=(self.global_position-pitch.global_position)
+	#print(a)
 	
 func _input(event:InputEvent) -> void:
 	if event.is_action_pressed("quit"):
@@ -41,15 +51,29 @@ func _input(event:InputEvent) -> void:
 		
 	if event.is_action_pressed("turn_left"):
 		self.rotation_degrees.y+=90.0
+		_view_changed()
 
 	if event.is_action_pressed("turn_right"):
 		self.rotation_degrees.y-=90.0
+		_view_changed()
 	
 	if event.is_action_pressed("speed_up"):
 		change_speed(movement_speed*2)
 	if event.is_action_released("speed_up"):
 		change_speed(movement_speed/2)
 
+func _view_changed():
+	var cp_dir:Vector3=(self.global_position-pitch.global_position)
+	if is_equal_approx(cp_dir.x,0) and cp_dir.z<0:
+		Global.player_change_view.emit("z_p")
+	if is_equal_approx(cp_dir.z,0) and cp_dir.x<0:
+		Global.player_change_view.emit("x_p")
+	if is_equal_approx(cp_dir.x,0) and cp_dir.z>0:
+		Global.player_change_view.emit("z_n")
+	if is_equal_approx(cp_dir.z,0) and cp_dir.x>0:
+		Global.player_change_view.emit("x_n")
+	print(cp_dir)
+		
 func change_speed(movement:int,jump:int=200,):
 	self_attri.attri["movement_speed"]=movement
 	self_attri.attri["jump_speed"]=jump
