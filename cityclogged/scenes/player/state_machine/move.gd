dd6aabca0dfdf60f7444c6d26bf83d568668337f
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2025-01-25 14:45:34 +0800

    写点小黑和player的东西

--- cityclogged/scenes/player/state_machine/move.gd
+++ cityclogged/scenes/player/state_machine/move.gd
@@ -1,7 +1,7 @@
 extends StateBase
 
 var movement_dir:Vector2
-signal to_idle(dir:Vector2,id:int)
+signal to_idle(dir:Vector2,id:String)
 
 func _enter():
 	super._enter()
@@ -13,17 +13,17 @@ func _exit():
 
 func _tick(delta:float):
 	super._tick(delta)
-	if not player.movement_direction:
+	if not owner.movement_direction:
 		state_machine.change_state("Idle")
 		an_t.get("parameters/playback").travel("Idle")
-		to_idle.emit(movement_dir,1)
+		to_idle.emit(movement_dir,self.name)
 	else:
-		movement_dir=player.movement_direction
+		movement_dir=owner.movement_direction
 
 func _physics_tick(delta:float):
 	super._physics_tick(delta)
-	an_t.set("parameters/Move/blend_position",player.movement_direction)
+	an_t.set("parameters/Move/blend_position",owner.movement_direction)
 	
-	player.velocity.x= player.movement_direction_3d.x*player.movement_speed*delta
-	player.velocity.z= player.movement_direction_3d.z*player.movement_speed*delta
+	owner.velocity.x= owner.movement_direction_3d.x*owner.movement_speed*delta
+	owner.velocity.z= owner.movement_direction_3d.z*owner.movement_speed*delta
 	pass
