d461e3180bff88a39bd010e59a909fed065ed664
aidewn <50902442+aidewn@users.noreply.github.com>
2025-01-26 16:40:31 +0800

    添加统计拥堵值得代码

--- cityclogged/module/3d_car/slow_down.gd
+++ cityclogged/module/3d_car/slow_down.gd
@@ -3,6 +3,7 @@ class_name SlowDownState
 extends State
 
 var ray_car: Car
+var is_jam:bool = false
 func enter():
 	print("进入刹车")
 func physics_update(delta: float) -> void:
@@ -13,13 +14,17 @@ func physics_update(delta: float) -> void:
 	if  car.ray_cast_3d.get_collider() is Car:
 		ray_car = car.ray_cast_3d.get_collider()
 		if ray_car.linear_velocity.length() < car.linear_velocity.length():
-			print("刹车")
 			if car.brake < car.max_velocity:
 				car.brake += delta * car.max_brake
+				
+				if car.linear_velocity.length() == 0 and !is_jam:
+					Global.jam_car_array += 1
+					is_jam = true
+				else:
+					Global.jam_car_array -= 1
+					is_jam = false
 			car.engine_force = 0
-			print(car.brake)
 		else:
-			print("松开")
 			car.brake = 0
 	
 	
