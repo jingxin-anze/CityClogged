a98ab373d5161fd6b375d04b7600b7091fa55fcd
aidewn <50902442+aidewn@users.noreply.github.com>
2025-02-11 20:57:54 +0800

    修改一个bug

--- cityclogged/module/GenerationCar/generation_car.gd
+++ cityclogged/module/GenerationCar/generation_car.gd
@@ -8,7 +8,11 @@ func create() -> void:
 	if car_scene and is_generation:
 		var car = car_scene.instantiate()
 		car.global_position = global_position
+		print(global_rotation)
+		
 		get_tree().root.add_child(car)
+		car.global_rotation = global_rotation
+		car.rotate_y(deg_to_rad(global_rotation.y))
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
